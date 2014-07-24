fd3ba63e0d1a16110347e043fb5e3a682973365a
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-07-24 00:57:13 -0700

    Change function name

--- mod_fileownercheck.c
+++ mod_fileownercheck.c
@@ -3,6 +3,7 @@
 // mod_fileownercheck.c - owner check for opened r->filename at output filter
 //
 // See Copyright Notice in LEGAL
+//
 */
 
 #include "httpd.h"
@@ -24,7 +25,7 @@
 
 module AP_MODULE_DECLARE_DATA fileownercheck_module;
 
-static int fileownercheck_check(request_rec *r, apr_file_t *fd)
+static int fileownercheck_from_file(request_rec *r, apr_file_t *fd)
 {
   apr_finfo_t finfo;
   struct stat st;
@@ -68,7 +69,7 @@ static apr_status_t fileownercheck_filter(ap_filter_t *f,
     return ap_pass_brigade(f->next, bb);
   }
 
-  status_code = fileownercheck_check(f->r, file->fd);
+  status_code = fileownercheck_from_file(f->r, file->fd);
   if (status_code != HTTP_OK) {
     apr_bucket *b;
 
